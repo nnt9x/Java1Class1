6f6ee63945641ef89c735c3203c697a424066d49
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-03-02 09:42:42 +0700

    Initial commit

---
+++ src/main/java/day1a/Example1.java
@@ -0,0 +1,25 @@
+package day1a;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        // In ra Hello world
+        // print line
+        System.out.println("Hello world");
+        // Thử đoạn mã sau và đưa nhận xét
+        System.out.print("Welcome to ");
+        System.out.print("Java");
+        // Thử và đưa nhận xét
+        String name = "BKACAD";
+        int year = 2023;
+        // Print format
+        System.out.printf("Xin chào các bạn đến với khoá học tại %s vào %d", name, year);
+
+        /*
+        Tạo 2 biến lưu thông tin tên và năm sinh (2000)
+        -> In ra tên và tuổi trên một dòng
+       
+         */
+
+    }
+}
