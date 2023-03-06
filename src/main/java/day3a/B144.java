024b9c26afe28b410f3e024e8f8cd92814daef51
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-03-06 11:54:49 +0700

    Day3 - Methods - Sửa bài tập B144

---
+++ src/main/java/day3a/B144.java
@@ -0,0 +1,38 @@
+package day3a;
+
+public class B144 {
+
+    // 2 hàm
+
+    public static boolean kiemTraNguyenTo(int n){
+        // Nguyên tố - true, ko nguyên tố là false
+        if(n <= 1) return false;
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static int soNguyenToDauTienTrongMang(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            // Kiem tra tung arr[i]
+            if(kiemTraNguyenTo(arr[i]) == true){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 8, 4, -5, 9, 11, 13, 30}; //11
+        int[] arr2 = {1, 4, 0, -4, 8, 10, 20};  // -1
+
+
+
+
+        System.out.println(soNguyenToDauTienTrongMang(arr1));
+        System.out.println(soNguyenToDauTienTrongMang(arr2));
+
+    }
+}
