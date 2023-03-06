031d5c6f9d86c5204b931d4e59466366f69e0182
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-03-06 14:26:10 +0700

    Day3 - OOP - Example 1

--- src/main/java/day3b/Example1.java
+++ src/main/java/day3b/Example1.java
@@ -1,5 +1,7 @@
 package day3b;
 
+import java.util.Scanner;
+
 public class Example1 {
     public static void main(String[] args) {
         // Tạo đối tượng d1
@@ -14,6 +16,17 @@ public class Example1 {
         d1.run();
         // Tạo đối tương d2 - dữ liẹu nhập từ bàn phím
         // Dùng các phương thức: run(), eat(), sleep()
+        Dog d2 = new Dog();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Nhập id: ");
+        d2.id = Integer.parseInt(in.nextLine());
+        System.out.print("Nhập tên: ");
+        d2.name = in.nextLine();
+        System.out.print("Nhập màu: ");
+        d2.color = in.nextLine();
 
+        d2.run();
+        d2.eat();
+        d2.sleep();
     }
 }
