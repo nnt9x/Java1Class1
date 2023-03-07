11b7bf9835e5df21caffa02c7718f4ce27eae61c
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-03-07 10:58:55 +0700

    Day4 - BT1.4

---
+++ src/main/java/oop/b1_4/Employee.java
@@ -0,0 +1,55 @@
+package oop.b1_4;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    public Employee(int id, String firstName, String lastName, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getName() {
+        return firstName + " " + lastName;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAnnualSalary() {
+        return salary * 12;
+    }
+
+    public int raiseSalary(int percent) {
+        // VD: 1000$ tang 10%
+        //
+        salary = salary + salary * percent / 100;
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee[" + "id=" + id + ",name='" + firstName + " " + lastName + ",salary=" + salary + ']';
+    }
+}
