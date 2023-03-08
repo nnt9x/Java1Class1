77df668bda1074e5dfaf88d2368e6141e6a55509
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-03-08 11:01:34 +0700

    Day5 - Tính trừu tượng - sửa bt 6_1

---
+++ src/main/java/oop/b6_1/Square.java
@@ -0,0 +1,37 @@
+package oop.b6_1;
+
+public class Square extends Rectangle {
+    public Square() {
+    }
+
+    public Square(double side) {
+        width = length = side;
+    }
+
+    public Square(double side, String color, Boolean filled) {
+        super(side, side, color, filled);
+    }
+
+    public double getSide() {
+        return length;// or width
+    }
+
+    public void setSide(double side) {
+        length = width = side;
+    }
+
+    @Override
+    public void setWidth(double side) {
+        setSide(side);
+    }
+
+    @Override
+    public void setLength(double side) {
+        setSide(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square[" + super.toString() + "]";
+    }
+}
