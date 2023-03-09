2e8b1c63583e94c230eb3a390ea5d531049e99a7
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-03-09 09:32:11 +0700

    Day6a - Sửa bài 2_6, 2_9

---
+++ src/main/java/oop/b2_6b2_9/MyPoint.java
@@ -0,0 +1,57 @@
+package oop.b2_6b2_9;
+
+public class MyPoint {
+    private int x = 0;
+    private int y = 0;
+
+    public MyPoint() {
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int[] getXY() {
+        return new int[]{x, y};
+    }
+
+    @Override
+    public String toString() {
+        // A(1, 1)
+        return String.format("(%d,%d)", x, y);
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y));
+    }
+    public double distance(MyPoint another){
+        return distance(another.x, another.y);
+    }
+
+    public double distance(){
+        return distance(0, 0);
+    }
+
+}
---
+++ src/main/java/oop/b2_6b2_9/TestMain2_9.java
@@ -0,0 +1,13 @@
+package oop.b2_6b2_9;
+
+public class TestMain2_9 {
+    public static void main(String[] args) {
+        MyPoint v1 = new MyPoint(0, 3);
+        MyPoint v2 = new MyPoint(4, 0);
+        MyPoint v3 = new MyPoint(4, 3);
+
+        Triangle triangle = new Triangle(v1, v2, v3);
+        System.out.println(triangle);
+        System.out.println(triangle.getPerimeter());
+    }
+}
