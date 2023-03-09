559275ce5dd4af28f19f3373163419203fd9ab79
NNT9x <42613109+nnt9x@users.noreply.github.com>
2023-03-09 11:55:12 +0700

    Day6a - Generic - Sua BT

---
+++ src/main/java/day6a/bt1/Main1.java
@@ -0,0 +1,13 @@
+package day6a.bt1;
+
+public class Main1 {
+    public static void main(String[] args) {
+        PhanSo ps1 = new PhanSo(3, 4);
+        PhanSo ps2 = new PhanSo(1, 2);
+
+        System.out.println(ps1.cong(ps2).rutGonPhanSo());
+        System.out.println(ps1.tru(ps2).rutGonPhanSo());
+        System.out.println(ps1.nhan(ps2).rutGonPhanSo());
+        System.out.println(ps1.chia(ps2).rutGonPhanSo());
+    }
+}
---
+++ src/main/java/day6a/bt1/MayTinh.java
@@ -0,0 +1,11 @@
+package day6a.bt1;
+
+public abstract class MayTinh <T> {
+    public abstract T tong(T...ts);
+
+    public abstract T hieu(T t1, T t2);
+
+    public abstract T tich(T...ts);
+
+    public abstract T thuong(T t1, T t2);
+}
